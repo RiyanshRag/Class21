b3ae3b125281a5f4a96e5b1ee14abbf959e56bb8
RiyanshRag <109984065+RiyanshRag@users.noreply.github.com>
2022-11-23 12:42:09 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+let engine;
+let world;
+
+var ground;
+var left;
+var right;
+var ball;
+
+function setup() {
+  createCanvas(1200,400);
+  engine = Engine.create();
+  world = engine.world;
+  
+  ground =new Ground(width/2,370,width,10);
+  left = new Ground(900,327,10,75)
+  right = new Ground(1050,327,10,75)
+
+  ball_options = {
+    isStatic:false,
+    restitution:0.1,
+    friction:0,
+    density:1.2
+  }
+
+  ball = Bodies.circle(100,100,10,ball_options);
+  fill("white");
+  World.add(world,ball);
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+function keyPressed() {
+  if (keyCode === (UP_ARROW)){
+    Matter.Body.applyForce(ball,{x:0,y:0},{x:18,y:-20})
+  }
+}
+
+function draw() 
+{
+  background("black");
+  ground.show();
+  left.show();
+  right.show();
+  Engine.update(engine);
+  ellipse(ball.position.x,ball.position.y,10);
+ }
+
